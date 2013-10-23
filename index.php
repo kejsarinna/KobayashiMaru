a3fea49e8709a08da616032e4ec4ae2fa5cfb4fa
Astrid R <kusin_tisdag@hotmail.com>
2013-10-23 20:31:07 +0200

    Test to see if it works to upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//
+// PHASE: BOOTSTRAP
+//
+define('KOBAYASHI_INSTALL_PATH', dirname(__FILE__));
+define('KOBAYASHI_SITE_PATH', KOBAYASHI_INSTALL_PATH . '/site');
+
+require(KOBAYASHI_INSTALL_PATH.'/src/CKobayashi/bootstrap.php');
+
+$kb = CKobayashi::Instance();
+
+//
+// PHASE: FRONTCONTROLLER ROUTE
+//
+$kb->FrontControllerRoute();
+
+
+//
+// PHASE: THEME ENGINE RENDER
+//
+$kb->ThemeEngineRender();
+
